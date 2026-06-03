e9a4d3bcb633b2c4e517ec40f91433032af54a34
Nogitsune00 <analuisa062007@gmail.com>
2026-06-03 19:31:03 -0300

    feat: adiciona o cadastro de pets para a adoção - Adiciona o cadastro do pet no banco de dados

--- client/src/pages/AddPet.jsx
+++ client/src/pages/AddPet.jsx
@@ -1,10 +1,59 @@
 import Navbar from "../components/Navbar";
+import React, {useState} from 'react';
+import Axios from "axios";
+
+const formularioInicial = {
+  nome: "",
+  tipo: "",
+  idade: "",
+  descricao: ""
+};
 
 export default function AddPet() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert("Pet cadastrado de forma simulada. Essa tela está protegida pelo login.");
+  const [formulario, setFormulario] = useState(formularioInicial);
+
+  function validateForm(){
+    if(!formulario.nome.trim()){
+      return "Informe o nome do pet"
+    }else if(!formulario.tipo.trim()){
+      return "Informe a especie do pet"
+    }else if(!Number(formulario.idade) || Number(formulario.idade)<0){
+      return "Informe uma idade valida para o pet"
+    }else if(!formulario.descricao){
+      return "Informe uma descrição para o pet"
+    }
+    return ""
   }
+
+  const handleChangeValues = (value) =>{
+    setFormulario(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value
+    }));
+  }
+
+  const handleClickButton = (event) => {
+    event.preventDefault()
+    const errorMessage = validateForm();
+
+    if(errorMessage){
+     alert(errorMessage);
+     return
+    }
+
+    Axios.post("http://localhost:3001/pet_adocao",{
+      nome: formulario.nome.trim(),
+      tipo: formulario.tipo.trim(),
+      idade: Number(formulario.idade),
+      descricao: formulario.descricao.trim()
+    }).then((response) => {
+      console.log(response);
+      setFormulario(formularioInicial);
+      document.getElementById('addPetAdocao').reset();
+      alert("Pet cadastrado com sucesso!");
+    })
+  }
+
 
   return (
     <>
@@ -15,12 +64,18 @@ export default function AddPet() {
 
       <main className="form-container">
         <h2>Preencha os detalhes abaixo para adicionar um pet à lista de adoção.</h2>
-        <form className="pet-form" onSubmit={handleSubmit}>
+        <form className="pet-form" id="addPetAdocao" onSubmit={handleClickButton}>
           <label htmlFor="nome">Nome do pet</label>
-          <input type="text" id="nome" name="nome" placeholder="Nome do pet" required />
+          <input 
+          type="text" 
+          id="nome" 
+          name="nome" 
+          placeholder="Nome do pet" 
+          required 
+          onChange={handleChangeValues}/>
 
           <label htmlFor="tipo">Tipo</label>
-          <select id="tipo" name="tipo" required defaultValue="">
+          <select id="tipo" name="tipo" required defaultValue="" onChange={handleChangeValues}>
             <option value="" disabled>Escolha...</option>
             <option>Cachorro</option>
             <option>Gato</option>
@@ -28,7 +83,13 @@ export default function AddPet() {
           </select>
 
           <label htmlFor="idade">Idade</label>
-          <input type="text" id="idade" name="idade" placeholder="Idade do pet" required />
+          <input 
+          type="text" 
+          id="idade" 
+          name="idade" 
+          placeholder="Idade do pet" 
+          required 
+          onChange={handleChangeValues}/>
 
           <label htmlFor="descricao">Descrição</label>
           <textarea
@@ -37,10 +98,8 @@ export default function AddPet() {
             placeholder="Descreva as características do pet"
             rows="4"
             required
+            onChange={handleChangeValues}
           ></textarea>
-
-          <label htmlFor="foto">Foto do pet</label>
-          <input type="file" className="form-control-file" id="foto" />
 
           <button type="submit">Adicionar Pet</button>
         </form>
