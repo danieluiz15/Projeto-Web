fab7c795e6f2672771990b64046e723d952ff1b1
Nogitsune00 <analuisa062007@gmail.com>
2026-06-03 23:17:28 -0300

    feat: adiciona a listagem de pets para adoção - Cria o componente PetAdocao para os cards de pet - Atualiza a página Pets para o uso de dados vindo do banco de dados - Atualiza a página Pets para o uso de um componente reutilizável - Atualiza o arquivo Banco.txt com instruções SQL de inserção de dados para testes

---
+++ client/src/components/PetAdocao.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+// style={{width: 18 + 'rem'}}
+export default function PetAdocao ({pet}){
+    return(
+        <article className="pet-card" key={pet.id}>
+            <div className="card-body">
+            <h5 className="card-title">{pet.nome}</h5>
+            <p className="card-text">
+                Idade: {pet.idade}, Especie: {pet.tipo}
+            </p>
+            <p>
+                Mais sobre mim: {pet.descricao}
+            </p>
+            <Link to="/declaracao" className="btn btn-info">
+                Quero adotar
+            </Link>
+            <div class="d-flex justify-content-center" style={{marginTop: 1 + 'vh', columnGap: 2 + '%'}}> 
+                <button className="btn btn-outline-secondary" type="button">
+                    Editar pet
+                </button>
+                <button className="btn btn-outline-danger" type="button">
+                    Excluir pet
+                </button>
+            </div>
+            </div>
+        </article>
+    );
+}
+
--- client/src/pages/Pets.jsx
+++ client/src/pages/Pets.jsx
@@ -1,34 +1,20 @@
-import { Link } from "react-router-dom";
+import PetAdocao from "../components/PetAdocao";
 import Navbar from "../components/Navbar";
-import alex from "../assets/img/alex.png";
-import sicha from "../assets/img/sicha.png";
-import mel from "../assets/img/Mel.png";
-import rosa from "../assets/img/Rosa.png";
-import bob from "../assets/img/bob.png";
-import will from "../assets/img/Will.png";
-import chavosa from "../assets/img/chavosa.png";
-import leo from "../assets/img/Leo.png";
-import joao from "../assets/img/Joao.png";
-import marias from "../assets/img/marias.png";
-import melo from "../assets/img/melo.png";
-import letImg from "../assets/img/let.png";
-
-const pets = [
-  { nome: "Alex", idade: "6 meses", local: "Brasília - DF", imagem: alex },
-  { nome: "Sicha", idade: "3 meses", local: "Brasília - DF", imagem: sicha },
-  { nome: "Mel", idade: "8 meses", local: "Brasília - DF", imagem: mel },
-  { nome: "Rosa", idade: "1 ano", local: "Brasília - DF", imagem: rosa },
-  { nome: "Bob", idade: "10 meses", local: "Brasília - DF", imagem: bob },
-  { nome: "Will", idade: "2 anos", local: "Brasília - DF", imagem: will },
-  { nome: "Chavosa", idade: "3 anos", local: "Brasília - DF", imagem: chavosa },
-  { nome: "Leo", idade: "6 meses", local: "Brasília - DF", imagem: leo },
-  { nome: "João", idade: "10 meses", local: "Brasília - DF", imagem: joao },
-  { nome: "Marias", idade: "1 ano", local: "Brasília - DF", imagem: marias },
-  { nome: "Melo", idade: "3 anos", local: "Brasília - DF", imagem: melo },
-  { nome: "Let", idade: "6 meses", local: "Brasília - DF", imagem: letImg }
-];
+import { useEffect, useState } from "react";
 
 export default function Pets() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/pet_adocao')
+      .then((response)=> response.json())
+      .then((data)=>{
+        setDados(data);
+      })
+      .catch((error)=> console.log("Erro ao buscar dados: ", error));
+  }, []);
+  console.log(`Dados: ${dados}`);
+
   return (
     <>
       <Navbar />
@@ -40,20 +26,16 @@ export default function Pets() {
 
         <h3 className="titulo-lista">Pets disponíveis</h3>
         <section className="pet-grid">
-          {pets.map((pet) => (
-            <article className="pet-card" key={pet.nome}>
-              <img className="card-img-top" src={pet.imagem} alt={`Foto do pet ${pet.nome}`} />
-              <div className="card-body">
-                <h5 className="card-title">{pet.nome}</h5>
-                <p className="card-text">
-                  {pet.idade}, {pet.local}
-                </p>
-                <Link to="/declaracao" className="btn btn-info">
-                  Quero adotar
-                </Link>
-              </div>
-            </article>
-          ))}
+          {dados.length > 0 ?(
+            dados.map((pet) => (
+              <PetAdocao
+                key={pet.id}
+                pet={pet}
+              />
+            ))
+          ) : (
+            <p>Nenhum pet cadastrado</p>
+          )}
         </section>
       </main>
     </>
