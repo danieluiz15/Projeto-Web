7412d65583a885d65a6c4261d7c32509f07ba422
AneliseBirk <anelisebirk@gmail.com>
2026-06-17 22:12:55 -0300

    Adiciona um botão para o relatório

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,16 +3,15 @@ import Navbar from "../components/Navbar";
 import PetsCrud from "./PetsCrud";
 import { AuthContext } from "../context/AuthContext";
 
+
 export default function Dashboard() {
   const { usuario } = useContext(AuthContext);
-
   return (
     <>
       <Navbar />
       <main className="page-content dashboard-page">
         <h1>Bem-vindo, {usuario.email}!</h1>
         <p>Gerenciamento de Pets</p>
-
         <PetsCrud />
       </main>
     </>
